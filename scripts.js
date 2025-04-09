57c2d2fae7868a4480359a2354840765bf4db3a9
Marcelo Freitas <markfreitaas@outlook.com>
2025-04-09 01:52:05 -0300

    alteração e inclusão planos

---
+++ scripts.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+      anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+    });
+  });
+
+  function enviarWhatsApp(event) {
+    event.preventDefault();
+
+    const emailInput = document.getElementById("email");
+    const phoneInput = document.getElementById("phone");
+
+    const email = emailInput.value;
+    const phone = phoneInput.value;
+
+    const msg = encodeURIComponent(`${email} e ${phone}`);
+    window.open(`https://wa.me/5551983117180?text=${msg}`, "_blank");
+
+    // Limpa os campos após o envio
+    emailInput.value = "";
+    phoneInput.value = "";
+  }
+
+  function abrirSuporte() {
+    const numero = "5551983117180"; // Substitua pelo número do suporte
+    const mensagem = encodeURIComponent(
+      "Olá! Preciso de ajuda com o TINDRA."
+    );
+    const url = `https://wa.me/${numero}?text=${mensagem}`;
+    window.open(url, "_blank");
+  }
